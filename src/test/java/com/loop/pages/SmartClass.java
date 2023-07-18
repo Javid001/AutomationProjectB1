ba74a416f2d90cc50c7eac9286925a44e15fd913
Javidshah <cavid597@gmail.com>
2023-07-17 22:51:07 -0400

    task1

--- src/test/java/com/loop/pages/SmartClass.java
+++
@@ -1,7 +0,0 @@
-package com.loop.pages;
-
-public class SmartClass {
-    public static void main(String[] args) {
-        System.out.println("Loop Academy");
-    }
-}
--- src/test/java/com/loop/pages/SmartClass2.java
+++
@@ -1,7 +0,0 @@
-package com.loop.pages;
-
-public class SmartClass2 {
-    public static void main(String[] args) {
-        System.out.println("Loop");
-    }
-}
