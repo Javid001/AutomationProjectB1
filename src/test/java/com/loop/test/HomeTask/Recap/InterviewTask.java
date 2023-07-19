3587e7dd61f822a47a5fb6b5bcc543e08be3c04e
Javidshah <cavid597@gmail.com>
2023-07-19 19:12:33 -0400

    task1

---
+++ src/test/java/com/loop/test/HomeTask/Recap/InterviewTask.java
@@ -0,0 +1,12 @@
+package com.loop.test.HomeTask.Recap;
+
+import java.util.Collection;
+import java.util.List;
+
+public class InterviewTask {
+
+    public static List<String>test(List<String> str){
+
+
+    }
+}
